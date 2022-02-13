7bcdfb67f216d7aa7de854a34da6f94f2098b85a
Pavithra <Pavithra@LAPTOP-ECJUFKE0>
2022-02-13 18:15:58 +0530

    feb12 commit

---
+++ src/com/demo/stringprint.java
@@ -0,0 +1,8 @@
+package com.demo;
+public class stringprint {
+	public static void main(String[] args) {
+		String fname="Java";
+		String lname="Course";
+		System.out.println(fname + " " + lname);
+	}
+}
